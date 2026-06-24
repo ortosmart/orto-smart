309c3be1cb880b78f6c77a3f4c29eed05bf3fbce
ortosmart <fisiega@hotmail.it>
2026-06-24 20:27:18 +0000

    Aggiunge progetto Flutter orto_app

---
+++ orto_app/lib/supabase_config.dart
@@ -0,0 +1,7 @@
+class SupabaseConfig {
+  static const String url =
+      'https://szjcriybaioyzsyelrfo.supabase.co';
+
+  static const String anonKey =
+      'sb_publishable_vOB3q-8_v5DPR9ZSFCdeAg_MzpHaE22';
+}
