29c7cfb39fc061c3c81cac293a25ac4024ddbbce
Renzo Siega <fisiega@hotmail.com>
2026-07-26 20:52:38 +0200

    feat: implement SuggestionEngine v1

---
+++ orto_app/lib/core/agronomy/models/free_space.dart
@@ -0,0 +1,18 @@
+class FreeSpace {
+  final int startCm;
+  final int lengthCm;
+
+  const FreeSpace({
+    required this.startCm,
+    required this.lengthCm,
+  });
+
+  int get endCm => startCm + lengthCm;
+
+  bool get isValid => lengthCm > 0;
+
+  @override
+  String toString() {
+    return 'FreeSpace($startCm - $endCm, length: $lengthCm cm)';
+  }
+}
